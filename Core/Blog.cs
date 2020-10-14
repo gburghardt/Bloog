80bd5c8e2a04014221005655b8799351fbb4cdde
Greg Burghardt <burghardt.greg@gmail.com>
2020-10-13 22:42:08 -0400

    Added comments to Blog class in Core. Used the word 'Netizen' for the first time. Didn't spell check it. Not even sure if people use it anymore.

--- Core/Blog.cs
+++ Core/Blog.cs
@@ -2,11 +2,18 @@
 
 namespace Bloog
 {
+    /// <summary>
+    /// Represents a collection of irreverant nonsense that spews forth from the frontal cortex of a Netizen.
+    /// </summary>
     public class Blog
     {
         private int Id { get; set; }
 
         private string name;
+
+        /// <summary>
+        /// The name of this blog.
+        /// </summary>
         public string Name
         {
             get => name;
@@ -21,8 +28,15 @@ namespace Bloog
             }
         }
 
+        /// <summary>
+        /// Event triggered when the name of this blog gets changed.
+        /// </summary>
         public event EventHandler<PropertyChangedEventArgs<int, string>> OnNameChanged;
 
+        /// <summary>
+        /// Initializes a new blog.
+        /// </summary>
+        /// <param name="name">The name of this new blog.</param>
         public Blog(string name)
         {
             if (string.IsNullOrWhiteSpace(name))
@@ -31,12 +45,21 @@ namespace Bloog
             Name = name;
         }
 
+        /// <summary>
+        /// Initializes a new blog object representing an existing blog.
+        /// </summary>
+        /// <param name="id">Identifier for this blog.</param>
+        /// <param name="name">Name of this blog.</param>
         public Blog(int id, string name) : this(name)
         {
             Id = id;
         }
 
-        public void ChangeName(string newName)
+        /// <summary>
+        /// Changes the name of this blog.
+        /// </summary>
+        /// <param name="newName">The new, cooler name.</param>
+        public void Rename(string newName)
         {
             if (string.IsNullOrWhiteSpace(newName))
                 throw new ArgumentNullException(nameof(newName), "New name cannot be empty.");
