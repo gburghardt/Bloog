f2ee6fef9bd7d1e9fec254142405f03fc1756eba
Greg Burghardt <burghardt.greg@gmail.com>
2020-10-15 22:56:48 -0400

    Added one integration test for UserGateway. Inserts. Need to find and assert.

---
+++ SqlServer.Tests/UserGatewayTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bloog.SqlServer.Tests
+{
+    public class UserGatewayTests
+    {
+        const string connectionString = @"Data Source=localhost\sqlexpress;Integrated Security=True";
+        [Fact]
+        public async void CreateNewUser()
+        {
+            var auditor = new StubAuditor(Guid.NewGuid(), () => DateTime.UtcNow);
+            var gateway = new UserGateway(auditor, connectionString);
+
+            await gateway.CreateUserAsync(auditor.UserId, auditor.UserId.ToString());
+        }
+    }
+}
--- SqlServer/UserGateway.cs
+++ SqlServer/UserGateway.cs
@@ -1,20 +1,29 @@
 ﻿using Bloog.Infrastructure;
+using Microsoft.Data.SqlClient;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Bloog.SqlServer
 {
     class UserGateway : BaseGateway, IUserGateway
     {
+        private const string InsertStatement = "INSERT into [dbo].[User] VALUES (@Id, @Username, @CreateUserId, @CreateDate, NULL, NULL)";
+
         internal UserGateway(IAuditor auditor, string connectionString) : base(auditor, connectionString)
         {
         }
 
-        public Task CreateUserAsync(Guid id, string username)
+        public async Task CreateUserAsync(Guid id, string username)
         {
-            throw new NotImplementedException();
+            using (var connection = await GetOpenConnectionAsync())
+            using (var command = new SqlCommand(InsertStatement, connection))
+            {
+                command.Parameters.AddWithValue("Id", id);
+                command.Parameters.AddWithValue("Username", username);
+                command.Parameters.AddWithValue("CreateUserId", Auditor.UserId);
+                command.Parameters.AddWithValue("CreateDate", Auditor.Now());
+                await command.ExecuteNonQueryAsync();
+            }
         }
 
         public Task<User> FindUserAsync(Guid id)
